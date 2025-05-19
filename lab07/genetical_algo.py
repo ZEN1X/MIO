21c9b189eca59ae5cca0b8000befb7198a2b5e31
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2025-05-19 10:50:45 +0200

    chore: formatting

--- lab07/genetical_algo.py
+++ lab07/genetical_algo.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
+
 class EncodingStyle(ABC):
     @abstractmethod
     def decode(self, chromosome: np.ndarray) -> float:
@@ -23,8 +24,9 @@ class BinaryEncoding(EncodingStyle):
     def decode(self, chromosome: np.ndarray) -> float:
         bits = self.turn_to_bits(chromosome)
         num = int(bits, 2)
-        
-        return self.x_min + num / (2**self.length-1) * (self.x_max - self.x_min)
+
+        return self.x_min + num / (2**self.length - 1) * (self.x_max - self.x_min)
+
 
 class GrayEncoding(EncodingStyle):
     def __init__(self, length: int, x_min: float = 0.01, x_max: float = 1.0) -> None:
@@ -32,7 +34,7 @@ class GrayEncoding(EncodingStyle):
         self.length = length
         self.x_min = x_min
         self.x_max = x_max
-    
+
     @staticmethod
     def gray_to_binary(gray: np.ndarray) -> np.ndarray:
         binary = gray.copy()
@@ -46,7 +48,8 @@ class GrayEncoding(EncodingStyle):
         bits = self.turn_to_bits(bin_bits)
         num = int(bits, 2)
 
-        return self.x_min + num / (2**self.length-1) * (self.x_max - self.x_min)
+        return self.x_min + num / (2**self.length - 1) * (self.x_max - self.x_min)
+
 
 class SelectionStyle(ABC):
     def __init__(self) -> None:
@@ -54,29 +57,39 @@ class SelectionStyle(ABC):
         self.generator = np.random.default_rng()
 
     @abstractmethod
-    def select(self, individuals: list['Individual'], fitness: np.ndarray) -> list['Individual']:
+    def select(
+        self, individuals: list["Individual"], fitness: np.ndarray
+    ) -> list["Individual"]:
         pass
+
 
 class RouletteSelection(SelectionStyle):
     def __init__(self) -> None:
         super().__init__()
 
-    def select(self, individuals: list['Individual'], fitness: np.ndarray) -> list['Individual']:
-        vals = fitness - np.min(fitness) + 1e-9 # min-shifted
-        probs = vals / np.sum(vals) # probabilities in range [0, 1]
-        
+    def select(
+        self, individuals: list["Individual"], fitness: np.ndarray
+    ) -> list["Individual"]:
+        vals = fitness - np.min(fitness) + 1e-9  # min-shifted
+        probs = vals / np.sum(vals)  # probabilities in range [0, 1]
+
         idx = self.generator.choice(len(individuals), size=len(individuals), p=probs)
         return [individuals[i].clone() for i in idx]
-    
+
+
 class ThresholdSelection(SelectionStyle):
     def __init__(self, gamma: float) -> None:
         super().__init__()
         self.gamma = gamma
 
-    def select(self, individuals: list['Individual'], fitness: np.ndarray) -> list['Individual']:
-        n = len(individuals) # total number of individuals
-        cutoff = int(np.ceil(self.gamma / 100 * n)) # number of individuals that will reproduce
-        top_idx = np.argsort(fitness)[-cutoff:] # top indices
+    def select(
+        self, individuals: list["Individual"], fitness: np.ndarray
+    ) -> list["Individual"]:
+        n = len(individuals)  # total number of individuals
+        cutoff = int(
+            np.ceil(self.gamma / 100 * n)
+        )  # number of individuals that will reproduce
+        top_idx = np.argsort(fitness)[-cutoff:]  # top indices
 
         chosen = self.generator.choice(top_idx, size=n)
         return [individuals[i].clone() for i in chosen]
